dbf7356f69f35ba7d78b74b5d205abf4fe69d502
Sukhonosov <Sukhonosov@bpcbt.com>
2018-09-04 15:09:56 +0300

    initial commit

---
+++ src/main/java/com/serg/PokerHand.java
@@ -0,0 +1,12 @@
+package main.java.com.serg;
+
+public class PokerHand implements Comparable {
+
+    public PokerHand(String hand) {
+    }
+
+    @Override
+    public int compareTo(final Object o) {
+        return 0;
+    }
+}
